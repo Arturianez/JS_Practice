35aabef8585e5e5e9e026034207f3a170b4d4bbd
Arthur Zankevich <artur.zank@gmail.com>
2024-06-01 23:04:36 -0400

    Tasks and web_dev

---
+++ Task_5/script.js
@@ -0,0 +1,50 @@
+// const obj = {
+//     name: 'obj name',
+//     exampleArr: [1, 2, 3, 4, 5, 'helllo', 'World', 'i am', 'on', 'fire']
+// }
+
+
+// //первая часть, где мы пишем ключевое слово function - это определение функции или же function declaration
+// function arrFilter(arr) {
+//     return arr
+//     .filter(item => typeof item === 'string')
+//     .map(item => item.split('').reverse().join(''))
+// }
+
+// console.log(arrFilter(obj.exampleArr))
+// console.log(arrFilter([1, 2, 3, 4, 6345, 'heheh', 'hscdv', 'yUry']))
+
+// function greetings(name) {
+//     console.log(Hello ${name} its your function)
+// }
+
+// function greetings2(name) {
+//     //функция всегда что - то возвращает
+//     return Hello world it is ${name}
+// }
+
+// function intCounter(a, b) {
+//     return a + b
+// }
+// console.log(intCounter(5, 10))
+// greetings('Yury')
+// greetings('Arthur')
+// greetings('king Arthur')
+// console.log(greetings2('Nikita'))
+
+
+// function fn(x, y, z)  {
+//     if(x > y) {
+//         return Math.pow(z, 2)
+//     }
+//     return x + y * z
+// }
+
+// console.log(fn(5, 2, 10))
+
+
+// function chooseCat(name,age,color) {
+//     return `my cat's name is ${name}, he/shes ${age} years old and he/she color ${color}`
+// }
+// console.log(chooseCat("Matt","13","grey"));
+// console.log(chooseCat("Barney","5","yellow/grey"));
---
+++ Task_6/script.js
@@ -0,0 +1,125 @@
+// Функция сложения двух чисел:
+// Напишите функцию add, которая принимает два числа и возвращает их сумму.
+
+// function add(a,b) {
+//     return a + b;
+// }
+// console.log('Sum of numbers: ', add(1,2));
+
+// Функция проверки четности числа:
+// Напишите функцию isEven, которая принимает число и возвращает true, если число четное, и false, если нечетное.
+
+// function isEven(num) {
+//     if(num % 2 == 0) {
+//         console.log("Even");
+//     } else {
+//         console.log("Uneven");
+//     }
+// }
+// isEven(2)
+
+// Функция нахождения максимального числа:
+// Напишите функцию max, которая принимает два числа и возвращает большее из них.
+
+// function max(a,b) {
+//     if(a > b) {
+//         return a;
+//     } else {
+//         return b;
+//     }
+// }
+// console.log('Max numbers is ', max(5,9));
+
+// Функция вычисления факториала:
+// Напишите функцию factorial, которая принимает число и возвращает его факториал.
+
+// function factorial(num) {
+//     if(num == 0 || num == 1) {
+//         return 1;
+//     } else {
+//         return num * factorial(num-1)
+//     }
+// }
+// console.log("Factorial number is ", factorial(5))
+
+
+// Функция переворота строки:
+// Напишите функцию reverseString, которая принимает строку и возвращает её в перевернутом виде.
+
+// function reverseString(someString) {
+//     const splitWord = someString.split("")
+//     const reversedWord = splitWord.reverse()
+//     const joinedWord = reversedWord.join("")
+//     return joinedWord;
+// }
+// console.log('Reverse string: ', reverseString("eno"));
+
+// Функция проверки палиндрома:
+// Напишите функцию isPalindrome, которая принимает строку и возвращает true, если строка является палиндромом, и false в противном случае.
+
+// function isPalindrome(word) {
+//     if(word.split("").reverse().join("") == word) {
+//         console.log("True");
+//     } else {
+//         console.log("False");
+//     }
+// }
+// isPalindrome("tot")
+
+// Функция вычисления площади круга:
+// Напишите функцию circleArea, которая принимает радиус круга и возвращает его площадь.
+
+// function circleArea(r) {
+//     return console.log(S = 3.14 * (r*r));
+// }
+// circleArea(5)
+
+// Функция проверки совершеннолетия:
+// Напишите функцию isAdult, которая принимает возраст и возвращает true, если возраст больше или равен 18, и false в противном случае.
+
+// function isAdult(age) {
+//     if(age >= 18) {
+//         console.log("True");
+//     } else {
+//         console.log("False");
+//     }
+// }
+// isAdult(34)
+
+// Функция поиска минимального числа в массиве:
+// Напишите функцию findMin, которая принимает массив чисел и возвращает минимальное число в массиве.
+
+// const arr = [35,78,22,101,29,56]
+// function findMin(someArr) {
+//    const minimum = Math.min(...someArr)
+//    return console.log(minimum);
+// }
+// findMin(arr)
+
+// Функция преобразования температуры:
+// Напишите функцию celsiusToFahrenheit, которая принимает температуру в градусах Цельсия и возвращает её в градусах Фаренгейта.
+
+// function celsiusToFahrenheit(temp) {
+//     const fahrenheit = temp*9/5+32
+//     return console.log("Fahrenheit: ", fahrenheit);
+// }
+// celsiusToFahrenheit(20)
+
+// Given an integer array nums, return true if any value appears at least twice in the array, 
+// and return false if every element is distinct.
+// Input: nums= [1, 2, 3, 4]
+// Output: false
+// Explanation: There are no duplicates in the given array.
+
+// const nums = [1,2,3,4]
+
+// function checkСoincidence(nums) {
+//     const numSet = new Set(nums) // create a bunch "new Set()" - all duplicates are automatically deleted
+//     return nums.length > numSet.size;
+// }
+// console.log(checkСoincidence(nums));
+
+
+
+
+
